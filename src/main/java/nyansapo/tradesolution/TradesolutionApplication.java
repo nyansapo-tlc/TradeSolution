06227219a889b4731205122291ce3773af426396
Innocent-Mawulorm-fiadu <you@example.com>
2020-11-10 07:58:59 -0800

    Spring Boot Project Created

---
+++ src/main/java/nyansapo/tradesolution/TradesolutionApplication.java
@@ -0,0 +1,13 @@
+package nyansapo.tradesolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradesolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradesolutionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nyansapo/tradesolution/TradesolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package nyansapo.tradesolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradesolutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
